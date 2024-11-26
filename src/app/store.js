1f4277ec604989c04e09d87850edc405d7f936ac
Georgi <chelchik90@gmail.com>
2024-11-26 17:00:36 +0400

    hidde-header

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,14 @@
 import { combineReducers, createStore } from "redux";
 import { InputValueReducer } from "../features/inputValueSlice";
 import { submitReducer } from "../features/SubmitSlice";
+import { themeReducer } from "../features/ThemeSlice";
+import { menuReducer } from "../features/menuSlice";
 
 const store = createStore(combineReducers({
     inputValue: InputValueReducer,
-    submit: submitReducer
+    submit: submitReducer,
+    theme: themeReducer,
+    menu: menuReducer
 }))
 
 export default store;
